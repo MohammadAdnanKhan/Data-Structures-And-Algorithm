c88782179e988ab9e7947a51e564b238fc9de636
MohdAdnanKhan001 <adnankhanmak1786@gmail.com>
2024-10-22 16:08:56 +0530

    Insert at beginning

---
+++ LinkedList/Insertatbeginning.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* insertAtBeginning(struct Node* head, int new_data) {
+    struct Node* new_node = (struct Node*) malloc(sizeof(struct Node));
+
+    new_node->data = new_data;
+
+    new_node->next = head;
+
+    return new_node;
+}
+
+void printList(struct Node* node) {
+    while (node != NULL) {
+        printf("%d -> ", node->data);
+        node = node->next;
+    }
+    printf("NULL\n");
+}
+
+int main() {
+    struct Node* head = NULL;
+
+    head = insertAtBeginning(head, 10);
+    head = insertAtBeginning(head, 20);
+    head = insertAtBeginning(head, 30);
+
+    printList(head);
+    return 0;
+}
